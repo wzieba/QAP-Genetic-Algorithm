195b0ffb5d3ad093cb9ab66b87e9fb7d0de23af8
wzieba <wojciech.zieba@protonmail.com>
2018-03-25 10:30:44 +0200

    Tournament selection added

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from src.crossover import perform_crossover
 from src.fitness_function import compute_fitness_scores_list
 from src.generate_population import generate_random_population
 from src.mutation import mutate_population
-from src.selection import generate_new_population_using_roulette_selection
+from src.tournament_selection import tournament_selection
 
 population = generate_random_population(matrices_size, 100)
 for epoch in range(100):
@@ -16,7 +16,7 @@ for epoch in range(100):
     print("Epoch: \t\t\t\t{}\nMean fit.: \t\t\t{}\nMax score: \t\t\t{}\nMax chrom.: \t\t{}\n"
           .format(epoch, np.mean(fitness_scores), max_fitness, max_chromosome))
 
-    selected_chromosomes = generate_new_population_using_roulette_selection(population, fitness_scores)
+    selected_chromosomes = tournament_selection(population, fitness_scores)
     crossed_chromosomes = perform_crossover(selected_chromosomes)
     mutated_chromosomes = mutate_population(crossed_chromosomes)
     print('\n')
---
+++ src/tournament_selection.py
@@ -0,0 +1,17 @@
+import random
+
+
+def tournament_selection(population_list, fitness_scores_list, elitism=True):  # Create new population
+    new_species = []
+    population_size = len(fitness_scores_list)
+    population_size = population_size - 1 if elitism else population_size
+    for _ in range(0, population_size):
+        # Take best
+        of_parent_idx = random.randint(0, len(fitness_scores_list) - 1)
+        tf_parent_idx = random.randint(0, len(fitness_scores_list) - 1)
+        if fitness_scores_list[of_parent_idx] > fitness_scores_list[tf_parent_idx]:
+            ch_winner = population_list[of_parent_idx]
+        else:
+            ch_winner = population_list[tf_parent_idx]
+        new_species.append(ch_winner)
+    return new_species
